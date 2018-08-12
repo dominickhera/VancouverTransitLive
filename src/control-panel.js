a8ba9260dfc854ee24893ef34937fea447b009b1
Dominick Hera <dominickhera@me.com>
2018-08-11 22:16:15 -0400

    making differentiation

--- src/control-panel.js
+++ src/control-panel.js
@@ -64,7 +64,7 @@ export default class ControlPanel extends PureComponent {
         .end((err, res) => {
             console.log(res.text);
             // this._xmlToJson(res.text);
-            console.log("update16");
+            console.log("update17");
             var parser = new DOMParser();
             var parsedObj = parser.parseFromString(res.text, "text/xml");
             // var jsonInstance = new X2JS();
@@ -75,7 +75,7 @@ export default class ControlPanel extends PureComponent {
             // console.log(parsedObj.getElementsByTagName("Bus")[0].childNodes[6].textContent);
            console.log("bus count is " + busCount);
            var i;
-            for(i = 0; i < (busCount - 1); i++) {
+            for(i = 0; i < 100; i++) {
               // console.log(i);
               console.log("item " + i + " : " + parsedObj.getElementById("Bus")[i].childNodes[6].textContent);
             }
