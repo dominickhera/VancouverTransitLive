442d8062a8df4d62f72e0a198fd779ffd809a679
Dominick Hera <dominickhera@me.com>
2018-08-11 20:13:23 -0400

    fixing preflight issue

--- src/control-panel.js
+++ src/control-panel.js
@@ -6,6 +6,44 @@ const request = require('superagent');
 const defaultContainer =  ({children}) => <div className="control-panel">{children}</div>;
 
 export default class ControlPanel extends PureComponent {
+  xmlToJson = (xml) => {
+	
+    // Create the return object
+    var obj = {};
+  
+    if (xml.nodeType == 1) { // element
+      // do attributes
+      if (xml.attributes.length > 0) {
+      obj["@attributes"] = {};
+        for (var j = 0; j < xml.attributes.length; j++) {
+          var attribute = xml.attributes.item(j);
+          obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+        }
+      }
+    } else if (xml.nodeType == 3) { // text
+      obj = xml.nodeValue;
+    }
+  
+    // do children
+    if (xml.hasChildNodes()) {
+      for(var i = 0; i < xml.childNodes.length; i++) {
+        var item = xml.childNodes.item(i);
+        var nodeName = item.nodeName;
+        if (typeof(obj[nodeName]) == "undefined") {
+          obj[nodeName] = xmlToJson(item);
+        } else {
+          if (typeof(obj[nodeName].push) == "undefined") {
+            var old = obj[nodeName];
+            obj[nodeName] = [];
+            obj[nodeName].push(old);
+          }
+          obj[nodeName].push(xmlToJson(item));
+        }
+      }
+    }
+    return obj;
+  };
+
   _grabTransitData = () => {
     // const request = require('superagent');
     //     const nocache = require('superagent-no-cache');
@@ -22,10 +60,13 @@ export default class ControlPanel extends PureComponent {
         .set('Content-Type', 'application/xml')
         .end((err, res) => {
             console.log(res.text);
+            var testThing = this.xmlToJson(res.text);
+            console.log(testThing);
         });
   }
   componentDidMount() {
     this._grabTransitData();
+    // this.setState
   }
 
   render() {
