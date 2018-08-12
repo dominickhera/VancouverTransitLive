b535e601ff900262bf838642797bf7175c6c0a6b
Dominick Hera <dominickhera@me.com>
2018-08-12 11:09:00 -0400

    gotta fix points

--- src/utils.js
+++ src/utils.js
@@ -2,8 +2,10 @@ export function pointOnCircle({center, angle, radius}) {
   return {
     type: 'Point',
     coordinates: [
-      center[0] + Math.cos(angle) * radius,
-      center[1] + Math.sin(angle) * radius
+      49.260900,
+      -123.113900,
+      // center[0] + Math.cos(angle) * radius,
+      // center[1] + Math.sin(angle) * radius
     ]
   };
 }
