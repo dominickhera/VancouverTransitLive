976257bc370b6425d277ceac269308fa6d14b898
Dominick Hera <dominickhera@me.com>
2018-08-11 22:13:52 -0400

    making differentiation

--- src/control-panel.js
+++ src/control-panel.js
@@ -64,7 +64,7 @@ export default class ControlPanel extends PureComponent {
         .end((err, res) => {
             console.log(res.text);
             // this._xmlToJson(res.text);
-            console.log("update15");
+            console.log("update16");
             var parser = new DOMParser();
             var parsedObj = parser.parseFromString(res.text, "text/xml");
             // var jsonInstance = new X2JS();
